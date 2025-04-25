36ca70d105a45869f807dff7cca8736018b42a6c
Ruben Petrosyan <ruben@Rubens-MacBook-Pro.local>
2025-04-24 17:35:20 -0700

    google

--- config/googleSheetsConfig.js
+++ config/googleSheetsConfig.js
@@ -1,18 +1,12 @@
+// googleSheetsConfig.js
 const { google } = require('googleapis');
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
-
-// Fix for private_key line breaks if needed
-if (serviceAccount.private_key.includes('\\n')) {
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-}
+const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
 
 const auth = new google.auth.JWT({
-  email: serviceAccount.client_email,
-  key: serviceAccount.private_key,
-  scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  email: credentials.client_email,
+  key: credentials.private_key,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 });
 
-const sheets = google.sheets({ version: 'v4', auth });
-
-module.exports = sheets;
+module.exports = auth;
